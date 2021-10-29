2b6b6d81f2489e7e61fb713f8f4dbf950075638c
DanielM21 <juan.maldonado-m@uniminuto.edu.co>
2021-10-29 13:03:08 -0500

    Full

---
+++ src/main/java/co/usa/ciclo3/ciclo3/web/ReservationController.java
@@ -0,0 +1,56 @@
+package co.usa.ciclo3.ciclo3.web;
+
+import co.usa.ciclo3.ciclo3.model.Reservation;
+import co.usa.ciclo3.ciclo3.reports.ClientCount;
+import co.usa.ciclo3.ciclo3.reports.StatusReservation;
+import co.usa.ciclo3.ciclo3.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/Reservation")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+
+public class ReservationController {
+    @Autowired
+    private ReservationService service;
+    @GetMapping("/all")
+    public List<Reservation> getReservations(){
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Reservation> getReservation(@PathVariable("id") int reservationId) {return service.getReservation(reservationId);}
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reservation save(@RequestBody Reservation reservation) {
+        return service.save(reservation);
+    }
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reservation update(@RequestBody Reservation reservation) {
+        return service.update(reservation);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable("id") int reservationId) {
+        return service.deleteReservation(reservationId);
+    }
+
+    @GetMapping("/report-status")
+    public StatusReservation getReservation (){
+        return service.getReportStatusReservation();
+    }
+
+    @GetMapping("/report-dates/{dateOne}/{dateTwo}")
+    public List<Reservation> getReservationTime (@PathVariable("dateOne")String dateOne, @PathVariable("dateTwo")String dateTwo){return service.getReportTimeReservation(dateOne,dateTwo);}
+
+    @GetMapping("/report-clients")
+    public List<ClientCount> getClient(){return service.serviceTopClient();}
+}
